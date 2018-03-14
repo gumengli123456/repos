5b15c43ff706559ee1e2ce0f78a43342df52b50a
gumengli <739913050@qq.com>
2018-03-14 11:23:56 +0800

    创建pod工程

---
+++ myLib/Classes/model/ssModel.h
@@ -0,0 +1,12 @@
+//
+//  ssModel.h
+//  myLib
+//
+//  Created by gumengli on 2018/3/14.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ssModel : NSObject
+
+@end
---
+++ myLib/Classes/model/ssModel.m
@@ -0,0 +1,12 @@
+//
+//  ssModel.m
+//  myLib
+//
+//  Created by gumengli on 2018/3/14.
+//
+
+#import "ssModel.h"
+
+@implementation ssModel
+
+@end
