243dadf571c5c0af9067a9af8f2e370d1bedcf80
gumengli <739913050@qq.com>
2018-03-15 11:00:00 +0800

    增加aaModel

---
+++ myLib/Classes/model/aaModel.h
@@ -0,0 +1,12 @@
+//
+//  aaModel.h
+//  myLib
+//
+//  Created by gumengli on 2018/3/15.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface aaModel : NSObject
+
+@end
---
+++ myLib/Classes/model/aaModel.m
@@ -0,0 +1,12 @@
+//
+//  aaModel.m
+//  myLib
+//
+//  Created by gumengli on 2018/3/15.
+//
+
+#import "aaModel.h"
+
+@implementation aaModel
+
+@end
